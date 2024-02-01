f82e6631a2c13f51c7e775f96a84e61e3f94e7a1
CamJak <cameron102601@gmail.com>
2024-02-01 10:17:56 -0600

    Updated admin homepage to display some demo config settings.

--- WebInterface/admin/home.php
+++ WebInterface/admin/home.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
+
+<?php
+    // DEBUG FILE LOCATION
+    $env = parse_ini_file('.\\setupVars.conf');
+    // $env = parse_ini_file('/etc/csna/setupVars.conf');
+    $ipAddress = $env['IP'];
+    $dns1 = $env['DNS1'];
+    $dns2 = $env['DNS2'];
+?>
+
 <html>
 <head>
     <title>Router Login</title>
@@ -7,5 +17,16 @@
 
 <body>
     <h1>Admin Homepage</h1>
+
+    <div class="container">
+        <h2>Router Configuration Settings</h2>
+        <?php if (!empty($ipAddress)) { ?>
+            <p>Router IP Address: <?php echo $ipAddress; ?></p>
+        <?php } if (!empty($dns1)) { ?>
+            <p>Router DNS1: <?php echo $dns1; ?></p>
+        <?php } if (!empty($dns2)) { ?>
+            <p>Router DNS2: <?php echo $dns2; ?></p>
+        <?php } ?>
+    </div>
 </body>
 </html>
