bbad7aefc63fa8295c21f3564287fac7dff93d5d
Blake <blake.perrin13@gmail.com>
2024-02-15 20:57:02 -0600

    AP Scan script is ready for demo

--- APSetup/Scripts/ap_scan.sh
+++ APSetup/Scripts/ap_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-cls_output=0
+cls_output=1
 json_output=1
 
 # This script is used to scan the available APs and display the results
@@ -37,6 +37,49 @@ fi
 
 if [ $json_output = 1 ] && [ $cls_output = 0 ]; 
 then
+    n=${#Channels[@]}
+    n=$((n-1))
+    j=1
+    for i in "${!Channels[@]}"; 
+    do
+        if [ $i -eq 0 ]; 
+        then
+            echo -e "{{ \"ap$i\": {\"ESSID\": $(echo $ESSID | awk -F',' '{print $'$j'}'), \"Frequency\": ${Frequencies[$i]}, \"Channel\": ${Channels[$i]}, \"Quality\": ${Qualities[$i]}, \"Signal\": ${Signals[$i]}}},"
+            j=$((j+1))
+        elif [ $i -eq $n ]; 
+        then
+            echo -e "{ \"ap$i\": {\"ESSID\": $(echo $ESSID | awk -F',' '{print $'$j'}'), \"Frequency\": ${Frequencies[$i]}, \"Channel\": ${Channels[$i]}, \"Quality\": ${Qualities[$i]}, \"Signal\": ${Signals[$i]}}}}"
+        else
+            echo -e "{ \"ap$i\": {\"ESSID\": $(echo $ESSID | awk -F',' '{print $'$j'}'), \"Frequency\": ${Frequencies[$i]}, \"Channel\": ${Channels[$i]}, \"Quality\": ${Qualities[$i]}, \"Signal\": ${Signals[$i]}}},"
+            j=$((j+1))
+        fi
+    done
+fi
+
+if [ $json_output = 1 ] && [ $cls_output = 1 ]; 
+then
+    echo ""
+    echo "Command Line Output"
+    echo ""
+
+    echo -e "ESSID\t\t\tFrequency\tChannel\tQuality\tSignal"
+    echo "-------------------------------------------------------------------------"
+    j=1
+    for i in ${!Channels[@]}; 
+    do
+        if [ $(echo $ESSID | awk -F',' '{print $'$j'}' | wc -c) -lt 18 ]; 
+        then
+            echo -e $(echo $ESSID | awk -F',' '{print $'$j'}')"\t\t"${Frequencies[$i]} GHz"\t\t"${Channels[$i]}"\t"${Qualities[$i]}"\t"${Signals[$i]} dBm
+        else
+            echo -e $(echo $ESSID | awk -F',' '{print $'$j'}')"\t"${Frequencies[$i]} GHz"\t\t"${Channels[$i]}"\t"${Qualities[$i]}"\t"${Signals[$i]} dBm
+        fi
+        j=$((j+1))
+    done
+
+    echo ""
+    echo "JSON Output"
+    echo ""
+
     n=${#Channels[@]}
     n=$((n-1))
     j=1
