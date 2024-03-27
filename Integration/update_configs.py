f4fa7040f3fe8af7698d20ce91770a1b12eafa69
Blake <blake.perrin13@gmail.com>
2024-03-26 19:40:15 -0500

    Added comments and reworked the code a bit

--- Integration/update_configs.py
+++ Integration/update_configs.py
@@ -5,16 +5,28 @@ phoebus_dhcp_file = "../APSetup/dnsmasq.d/phoebus-dhcp.conf"
 dns_file = "dns.conf"
 dhcp_file = "dhcp.conf"
 
+### Function to write data to the dns and dhcp files
+# data: dictionary containing the data to be written
+# type: the syntax that dnsmasq uses for its config file
+# file: the output file that the data will be written to 
 def write_data(data, type, file):
+
+    # Open the file in append mode
     with open(file, 'a') as f:
+
+        # Functionality for the type: Domain 
         if type == "domain":
             f.write(f"{type}={data}\n")
             f.write(f"local=/{data}/\n")
+
+        # Functionality for the type: dhcp-range (creates the subnet)
         elif type == "dhcp-range":
             for key in data:
                 output = f"{type}={key},{data[key]['Start']},{data[key]['End']},{data[key]['Mask']},{data[key]['Lease']}\n"
-                output += f"{type}={key},3,{data[key]['Router']}"
+                output += f"dhcp-option={key},3,{data[key]['Router']}"
                 f.write(f"{output}\n")
+
+        # Functionality for the boolean options
         elif type == "dhcp-option":
             for key in data:
                 if data[key] == "True":
@@ -22,84 +34,101 @@ def write_data(data, type, file):
                     f.write(f"{output}\n")
                 else:
                     f.write("")
+
+        # For any other type use the format: type=data
         else:
             for key in data:
                 output = f"{type}={data[key]}"
                 f.write(f"{output}\n")
 
+        f.close()
+
+### Function to erase data from the dns and dhcp files
+# file: the file to be erased
+# option: specifies if the dns or dhcp data should be rewritten to the file
+# otherwise the entire file will be erased
 def erase_data(file, option=None):
-    if option == "dhcp":
-        with open(file, 'w') as f:
+
+    with open(file, 'w') as f:
+        # If the option is dhcp, write the unchangeable dhcp options back to the file.
+        if option == "dhcp":
             f.write("dhcp-leasefile=/etc/phoebus/dhcp.leases\n")
-            f.close()
-    elif option == "dns":
-        with open(file, 'w') as f:
+    
+        # If the option is dns, write the unchangeable dns options back to the file.
+        elif option == "dns":
             output = "localise-queries\nno-resolv\ndomain-needed\nexpand-hosts\nbogus-priv\n"
             output += "log-queries\nlog-async\nlog-facility=/var/log/phoebus/phoebus.log\n"
             output += "addn-hosts=/etc/phoebus/local.list\naddn-hosts=/etc/phoebus/custom.list\n"
             output += "dhcp-ignore-names=set:hostname-ignore\ndhcp-name-match=set:hostname-ignore,wpad\n"
             output += "dhcp-name-match=set:hostname-ignore,localhost\ncache-size=10000\n"
             f.write(output)
-            f.close()
-    else:
-        with open(file, 'w') as f:
-            f.write("")
-            f.close()
 
-def get_data(config_data):
+        # If the option is none, erase the entire file
+        else:
+            f.write("")
+
+        f.close()
+
+### Function to get the data from the config file
+# config_file: the file to get the data from
+def get_data(config_file):
+
     ifaces = {}
     dns_servers = {}
     dhcp_options = {}
     subnets = {}
     domain_name = None
 
-    is_subnet = False
-    subnet_name = None
-    for key in config_data:
-        if "IFACE" in key:
-            ifaces[key] = config_data[key]
-        
-        if "DNS" in key:
-            dns_servers[key] = config_data[key]
-        
-        if "DHCP_" in key:
-            dhcp_options[key] = config_data[key]
-        
-        if "SUBNET" in key:
-            subnet_name = config_data[key]
-            subnets[subnet_name] = {}
-            is_subnet = True
+    # Open the config file in read mode
+    with open(config_file, "r") as f:
 
-        if "_START" in key and is_subnet:
-            subnets[subnet_name]["Start"] = config_data[key]
+        # variables to check if a subnet has been initialized
+        is_subnet = False
+        subnet_name = None
 
-        if "_END" in key and is_subnet:
-            subnets[subnet_name]["End"] = config_data[key]
-        
-        if "_MASK" in key and is_subnet:
-            subnets[subnet_name]["Mask"] = config_data[key]
+        for line in f:
+            key = line.split('=')[0]
+            value = line.split('=')[1].strip("\n")
 
-        if "_ROUTER" in key and is_subnet:
-            subnets[subnet_name]["Router"] = config_data[key]
-        
-        if "_LEASE" in key and is_subnet:
-            subnets[subnet_name]["Lease"] = config_data[key]
-            is_subnet = False
+            if "IFACE" in key:
+                ifaces[key] = value
+            
+            if "DNS" in key:
+                dns_servers[key] = value
+            
+            if "DHCP_" in key:
+                dhcp_options[key] = value
+            
+            if "SUBNET" in key:
+                subnet_name = value
+                subnets[subnet_name] = {}
+                is_subnet = True
 
-        if "DOMAIN" in key:
-            domain_name = config_data[key]
+            if "_START" in key and is_subnet:
+                subnets[subnet_name]["Start"] = value
+
+            if "_END" in key and is_subnet:
+                subnets[subnet_name]["End"] = value
+            
+            if "_MASK" in key and is_subnet:
+                subnets[subnet_name]["Mask"] = value
+
+            if "_ROUTER" in key and is_subnet:
+                subnets[subnet_name]["Router"] = value
+            
+            if "_LEASE" in key and is_subnet:
+                subnets[subnet_name]["Lease"] = value
+                is_subnet = False
+
+            if "DOMAIN" in key:
+                domain_name = value
+
+        f.close()
 
     return ifaces, dns_servers, dhcp_options, subnets, domain_name
 
-with open(gui_config_file, "r") as f:
-    config_data = {}
-    for line in f:
-        key = line.split('=')[0]
-        value = line.split('=')[1]
-        config_data[key] = value.strip("\n")
-    f.close()
-
-ifaces, dns_servers, dhcp_options, subnets, domain_name = get_data(config_data)
+# Get data from the config file
+ifaces, dns_servers, dhcp_options, subnets, domain_name = get_data(gui_config_file)
 
 # Write data to dns file
 erase_data(dns_file, "dns")
